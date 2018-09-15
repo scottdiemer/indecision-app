15c2d14b70678dcfe69c68c828a8bc4e03e610fc
scottdiemer <sdiemer@cfl.rr.com>
2018-09-15 08:55:09 -0400

    Added saving and loading of data using local storage

--- src/app.js
+++ src/app.js
@@ -13,11 +13,23 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('fetching data');
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if(options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('saving data');
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
   }
 
   componentWillUnmount() {
@@ -108,6 +120,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {
         props.options.map((option) => (
           <Option
@@ -152,6 +165,9 @@ class AddOption extends React.Component {
 
     this.setState(() => ({ error }));
     
+    if(!error) {
+      e.target.elements.option.value = '';
+    }
   }
   render() {
     return (
