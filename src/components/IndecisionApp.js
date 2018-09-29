cf86ed3e3a39ffb2fba77e01404afb7df826eb58
scottdiemer <sdiemer@cfl.rr.com>
2018-09-28 23:32:17 -0400

    added theming with sass

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -80,43 +80,23 @@ export default class IndecisionApp extends React.Component {
 
   render() {
     const subtitle = 'Put your life in the hands of a computer';
-    return ( <div>
-      <Header subtitle={
-        subtitle
-      }
-      /> 
-      {' '}
-      <Action
-        hasOptions={
-        this.state.options.length > 0
-      }
-        handlePick={
-        this.handlePick
-      }
-      /> 
-      {' '}
-      <Options
-        options={
-        this.state.options
-      }
-        handleDeleteOptions={
-        this.handleDeleteOptions
-      }
-        handleDeleteOption={
-        this.handleDeleteOption
-      }
-      /> 
-      {' '}
-      <AddOption handleAddOption={
-        this.handleAddOption
-      }
-      />
-      <OptionModal
-        selectedOption={this.state.selectedOption}
-        handleClearSelectedOption={this.handleClearSelectedOption}
-      />
- 
-    </div>
+    return ( 
+      <div>
+        <Header subtitle={subtitle} /> 
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick} /> 
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption} /> 
+          <AddOption handleAddOption={this.handleAddOption} />
+          <OptionModal
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption} />
+        </div>
+      </div>
     );
   }
 }
