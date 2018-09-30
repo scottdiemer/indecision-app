a2efd2bb65b866d38789b2806b65787cfb3175fc
scottdiemer <sdiemer@cfl.rr.com>
2018-09-29 23:01:01 -0400

    added more scss

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,9 +4,10 @@ const Option = (props) => (
   <div> 
     {props.optionText} 
     <button
+      className="button button--link"
       onClick={
         (e) => {props.handleDeleteOption(props.optionText);}
-    }
+      }
     >
     remove
     </button>
